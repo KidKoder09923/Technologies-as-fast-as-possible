f29b7941eaafe35ec3209cc48ee0a9d1f80fdfbf
Kid Koder <codermann09@gmail.com>
2021-06-06 10:07:58 -0500

    Added comments to TS

--- ts/typescript.ts
+++ ts/typescript.ts
@@ -1,7 +1,6 @@
+//The colon `:` allows the TypeScript to know what type to use
 function toString(input: number) : string {
     return `'${input.toString()}'`;
 }
 
 console.log(toString(12))
-
-const thingu = null;
